bc9a4194bf3da16d009fb564ae20c57893c5299d
Rruiz21 <oskar.ruiz21@hotmail.es>
2025-01-03 10:18:20 -0600

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const fechaBoda = new Date('2025-09-21T15:30:00').getTime();
+const fechaBoda = new Date("2025-09-21T15:30:00").getTime();
 
-const actualizarContador = () => {
+function actualizarContador() {
     const ahora = new Date().getTime();
     const diferencia = fechaBoda - ahora;
 
@@ -10,13 +10,13 @@ const actualizarContador = () => {
         const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
         const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
 
-        document.getElementById('dias').innerText = dias;
-        document.getElementById('horas').innerText = horas;
-        document.getElementById('minutos').innerText = minutos;
-        document.getElementById('segundos').innerText = segundos;
+        document.getElementById("dias").textContent = dias;
+        document.getElementById("horas").textContent = horas;
+        document.getElementById("minutos").textContent = minutos;
+        document.getElementById("segundos").textContent = segundos;
     } else {
-        document.getElementById('contador').innerHTML = '<h3>¡Es nuestro día especial!</h3>';
+        document.getElementById("contador").innerHTML = "<h3>¡Es nuestro día especial!</h3>";
     }
-};
+}
 
 setInterval(actualizarContador, 1000);
